92a9314b6ac23e4b900fb0a1cec9c1a0daee5870
GeoFernandes <geovannaferoliveira@gmail.com>
2023-10-03 17:55:48 -0300

    Header login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { VitrineComponent } from './vitrine/vitrine.component';
 import { ProdutoDetalheComponent } from './components/produto-detalhe/produto-detalhe.component';
 import { ProdutoBuscaComponent } from './components/produto-busca/produto-busca.component';
 import { CestaComponent } from './components/cesta/cesta.component';
 import { LoginComponent } from './components/login/login.component';
 import { EsqueciComponent } from './components/esqueci/esqueci.component';
+import { VitrineComponent } from './components/vitrine/vitrine.component';
 
 @NgModule({
   declarations: [
