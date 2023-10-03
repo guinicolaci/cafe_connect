8fca6dd5a9120d692d9fce2d9217c62dd7ba4801
Geovanna Fernandes <108410133+GeoFernandes@users.noreply.github.com>
2023-10-03 20:39:34 -0300

    Revert "Feature/login senha"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CestaComponent } from './components/cesta/cesta.component';
-import { EsqueciComponent } from './components/esqueci/esqueci.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProdutoBuscaComponent } from './components/produto-busca/produto-busca.component';
-import { ProdutoDetalheComponent } from './components/produto-detalhe/produto-detalhe.component';
-import { VitrineComponent } from './components/vitrine/vitrine.component';
-
-const routes: Routes = [
-  { path: '', component: VitrineComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'senha', component: EsqueciComponent },
-  { path: 'busca', component: ProdutoBuscaComponent },
-  { path: 'detalhe', component: ProdutoDetalheComponent },
-  { path: 'cesta', component: CestaComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
